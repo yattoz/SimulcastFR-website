5f29b9ec8a2b62ddfef96fec86df0485ab18b5c2
simulcastfr <none@none.non>
2019-09-03 01:40:11 +0200

    added calendar

--- src/components/APropos.vue
+++ src/components/APropos.vue
@@ -1,9 +1,20 @@
 <template>
-    <div class="uk-align-left">
-        <h1>A propos</h1>
+    <div class="left" align="left">
+        <h3>A propos</h3>
         <p>
             Ceci est un site affichant les simulcasts des diffuseurs saisonniers principaux français.
-            Il a la particularité de mettre à jours ses données automatiquement.
+            Il a la particularité d'utiliser des données automatiquement récupérées des sites de simulcast.
+        </p>
+        <h3>Contact</h3>
+        <p>
+            Mon Twitter est ouvert : <a href=https://www.twitter.com/yattoz>@yattoz</a> pour :
+            <ul>
+                <li>- Problèmes,</li>
+                <li>- Bugs,</li>
+                <li>- Messages d'encouragement,</li>
+                <li>- Propositions de me filer un coup de main,</li>
+                <li>- Suggestions (notamment sur l'ergonomie, je fais ça au flair).</li>
+            </ul>
         </p>
     </div>
 </template>
--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,38 +1,61 @@
 <template>
+
     <div class="">
         <h1>En construction !</h1>
-         <button class="btn" @click="toggleWeekends">toggle weekends</button>
+        <p>Ce calendrier contient des données fixes pour démonstration.</p>
                 <FullCalendar
                         class="demo-app-calendar"
                         ref="fullCalendar"
-                        defaultView="timeGridWeek"
-
+                        defaultView="listWeek"
+                        weekNumberCalculation="ISO"
+                        :views="{
+                            timeGridWeek: { buttonText: 'Semaine (calendrier)' },
+                            listWeek: { buttonText: 'Semaine (liste)' },
+                            listDay: { buttonText: 'Aujourd\'hui (liste)' }
+                        }"
                         :header="{
                             left: '',
                             center: 'Simulcast Ete 2019',
-                            right: 'timeGridWeek,timeGridDay'
+                            right: 'timeGridWeek,listWeek,listDay'
                           }"
                         :plugins="calendarPlugins"
                         :weekends="calendarWeekends"
                         :events="calendarEvents"
                         :locales="['fr']"
-                        :height="100"
+                        :height="'auto'"
+                        @eventClick="eventClick"
                         @dateClick="handleDateClick"
                 />
     </div>
 </template>
 
 <script>
+    import JQuery from 'jquery'
+    let $ = JQuery;
+    import StoreFilter from '@/components/StoreFilter';
+
     import FullCalendar from "@fullcalendar/vue";
     import dayGridPlugin from "@fullcalendar/daygrid";
     import timeGridPlugin from "@fullcalendar/timegrid";
     import interactionPlugin from "@fullcalendar/interaction";
     import frLocale from '@fullcalendar/core/locales/fr';
+    import listPlugin from '@fullcalendar/list';
 
     // must manually include stylesheets for each plugin
     import "@fullcalendar/core/main.css";
     import "@fullcalendar/daygrid/main.css";
     import "@fullcalendar/timegrid/main.css";
+    import "@fullcalendar/list/main.css";
+
+    /*
+    const cr_lineup_url = "json/cr_calendar.json";
+    const adn_lineup_url = "json/adn_calendar.json";
+    const waka_lineup_url = "json/waka_calendar.json";
+
+     */
+    const full_calendar_url = "json/full_calendar.json";
+    const proxy = 'https://jsonp.afeld.me/?url=https://simulcastfr.netlify.com/';
+    //const proxy = '';
 
     export default {
         components: {
@@ -44,26 +67,61 @@
                     // plugins must be defined in the JS
                     dayGridPlugin,
                     timeGridPlugin,
-                    interactionPlugin // needed for dateClick
+                    interactionPlugin, // needed for dateClick
+                    listPlugin
                 ],
                 calendarWeekends: true,
                 calendarEvents: [
-                    // initial event data
-                    { title: "Event Now", start: new Date() }
-                ]
+                    // initial event dat
+                ],
+                
+                full_calendar_url: "json/full_calendar.json",
+                full_calendar: [],
+                FilterResults: StoreFilter.state
+
             };
         },
         mounted() {
+            /*
             this.calendarEvents.push({
                 // add new event data
                 title: "New Event",
                 start: new Date("2019-"),
                 allDay: false
             });
+            */
+            var self = this;
+            $.getJSON(proxy + self.full_calendar_url, function (json) {
+                self.full_calendar = json;
+                self.full_calendar.forEach(function(anime){
+                    self.calendarEvents.push({
+                        // add new event data
+                        title: anime.title + ' ep. ' + anime.ep_number,
+                        start: new Date(anime.ep_time),
+                        url: anime.ep_link,
+                        backgroundColor: self.badgeColor(anime),
+                        allDay: false
+                    });
+                    
+                });
+                console.log(self.calendarEvents);
+            })
         },
         methods: {
             toggleWeekends() {
                 this.calendarWeekends = !this.calendarWeekends; // update a property
+            },
+            badgeColor(anime) {
+                let service = anime.service;
+                service = service.toLowerCase();
+                if (service === "crunchyroll")
+                    return "#df6400d0";
+                if (service === "adn")
+                    return "#0066ff";
+                if (service === "wakanim")
+                    return "#e0000a";
+                // catch-all
+                return "#fefefe";
             },
             handleDateClick(arg) {
                 if (confirm("Would you like to add an event to " + arg.dateStr + " ?")) {
@@ -73,6 +131,13 @@
                         start: arg.date,
                         allDay: arg.allDay
                     });
+                }
+            },
+            eventClick: function(info) {
+                info.jsEvent.preventDefault(); // don't let the browser navigate
+
+                if (info.event.url) {
+                window.open(info.event.url);
                 }
             }
         }
@@ -87,6 +152,7 @@
 
     .demo-app-top {
         margin: 0 0 3em;
+        color: #df6400c5;
 
     }
 
@@ -94,4 +160,5 @@
         margin: 0 auto;
         max-width: 900px;
     }
+    
 </style>
