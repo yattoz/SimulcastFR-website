d1f1bc5312fa022ff4eb1d9b44309579f702eae6
yattoz <yatto_963@hotmail.fr>
2020-04-07 00:20:54 +0200

    added long title, shadow

--- src/components/NavHead.vue
+++ src/components/NavHead.vue
@@ -1,47 +1,52 @@
 <template>
   <!-- <div uk-sticky="sel-target: .uk-navbar-container; cls-active: uk-navbar-sticky"> -->
   <div>
-    <nav class="nav-extended">
-      <div class="nav-wrapper purple ">
-        <div class="nav-content">
-        <router-link class="brand-logo smart" to="/">SimulcastFR</router-link>
-        <a href="#" data-target="mobile-demo" class="sidenav-trigger">
-          <i class="material-icons">menu</i>
-        </a>
-        <ul class="tabs tabs-transparent hide-on-med-and-down">
-          <li class="tab" @click="setPath('/')" id="/">
-            <router-link :class="this.path == '/' ? 'active' : ''" to="/" >Simulcasts de la saison</router-link>
-          </li>
+    <nav class="nav-extended  z-depth-3">
+      <div class="nav-wrapper purple">
+        <div class="nav-content ">
+          
+          <router-link class="brand-logo smart" to="/" ><span @click="setPath('/')" >SimulcastFR
+          <span class="hide-on-med-and-down"> —  le catalogue de l'animation simulcast</span></span></router-link>
+          <a href="#" data-target="mobile-demo" class="sidenav-trigger">
+            <i class="material-icons">menu</i>
+          </a>
+          <ul class="tabs tabs-transparent hide-on-med-and-down">
+            <li class="tab" @click="setPath('/')" id="/">
+              <router-link :class="this.path == '/' ? 'active' : ''" to="/">Simulcasts de la saison</router-link>
+            </li>
             <li class="tab" @click="setPath('/calendar')" id="/calendar">
-            <router-link :class="this.path == '/calendar' ? 'active' : ''" to="/calendar" >Calendrier</router-link>
-          </li>
-          <li class="tab" @click="setPath('/full-lineup')" id="/full-lineup">
-            <router-link class="" to="/full-lineup" >Catalogue</router-link>
-          </li>
-          <li class="tab" @click="setPath('/added-removed')" id="/added-removed">
-            <router-link class="" to="/added-removed" >Ajouts / Retraits</router-link>
-          </li>
-          <li class="tab" @click="setPath('/about')" id="/about">
-            <router-link :class="this.path == '/about' ? 'active' : ''" to="/about">A propos</router-link>
-          </li>
-        </ul>
+              <router-link
+                :class="this.path == '/calendar' ? 'active' : ''"
+                to="/calendar"
+              >Calendrier</router-link>
+            </li>
+            <li class="tab" @click="setPath('/full-lineup')" id="/full-lineup">
+              <router-link class to="/full-lineup">Catalogue</router-link>
+            </li>
+            <li class="tab" @click="setPath('/added-removed')" id="/added-removed">
+              <router-link class to="/added-removed">Ajouts / Retraits</router-link>
+            </li>
+            <li class="tab" @click="setPath('/about')" id="/about">
+              <router-link :class="this.path === '/about' ? 'active' : ''" to="/about">A propos</router-link>
+            </li>
+          </ul>
         </div>
       </div>
     </nav>
     <ul class="sidenav" id="mobile-demo">
-      <li>
+      <li @click="setPath('/')" :class="this.path == '/' ? 'active' : ''" to="/">
         <router-link class="waves-effect sidenav-close" to="/">Simulcasts de la saison</router-link>
       </li>
-      <li>
-      <router-link class="waves-effect sidenav-close" to="/calendar">Calendrier</router-link>
-          </li>
-      <li>
+      <li @click="setPath('/calendar')" :class="this.path == '/calendar' ? 'active' : ''" to="/">
+        <router-link class="waves-effect sidenav-close" to="/calendar">Calendrier</router-link>
+      </li>
+      <li @click="setPath('/full-lineup')" :class="this.path == '/full-lineup' ? 'active' : ''" to="/">
         <router-link class="waves-effect sidenav-close" to="/full-lineup">Catalogue</router-link>
       </li>
-      <li>
+      <li @click="setPath('/added-removed')" :class="this.path == '/' ? 'added-removed' : ''" to="/">
         <router-link class="waves-effect sidenav-close" to="/added-removed">Ajouts / Retraits</router-link>
       </li>
-      <li>
+      <li @click="setPath('/about')" :class="this.path == '/' ? 'active' : ''" to="/">
         <router-link class="waves-effect sidenav-close" to="/about">A propos</router-link>
       </li>
     </ul>
@@ -62,34 +67,40 @@ export default {
   methods: {
     setPath: function(str) {
       this.path = str;
+      var el = document.querySelector(".tabs");
+      var instance = M.Tabs.getInstance(el);
+      console.log(el);
+      console.log(instance);
+
+      instance.updateTabIndicator();
+      instance.select(this.path);
+      instance.updateTabIndicator();
       console.log("Path = " + this.path);
     }
   },
   mounted() {
     document.addEventListener("DOMContentLoaded", function() {
-      var elems = document.querySelectorAll(".sidenav");
+      var elems = document.querySelector(".sidenav");
       var options = { edge: "left" };
       M.Sidenav.init(elems, options);
 
-      var el = document.querySelectorAll(".tabs");
+      var el = document.querySelector(".tabs");
       console.log(el);
-      options = { };
+      options = {};
       var instance = M.Tabs.init(el, options);
       console.log(instance);
 
       this.path = window.location.pathname;
       console.log(this.path);
 
-      instance[0].select(this.path);
-      instance[0].updateTabIndicator();
-
+      instance.select(this.path);
+      instance.updateTabIndicator();
     });
   }
 };
 </script>
 
 <style scoped>
-
 /*
 .{
   color: #fefefe;
@@ -104,8 +115,4 @@ export default {
     position: relative;
   }
 }
-
-
-
-
 </style>
